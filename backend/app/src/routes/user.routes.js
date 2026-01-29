00c3983e940249b92481b51a810c624747f7867a
nigam <244813753+nigam-raval@users.noreply.github.com>
2026-01-29 06:58:45 +0530

    refactor: remove legacy code - remove unused multer middlware file - remove multer middleware from user routes - uninstall multer dependencies

--- backend/app/src/routes/user.routes.js
+++ backend/app/src/routes/user.routes.js
@@ -1,5 +1,4 @@
 import { Router } from "express";
-import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/authentication.middleware.js";
 import { 
     changeCurrentPassword, 
@@ -16,23 +15,10 @@ import {
     deleteUser,
     generateStsCredentials
 } from "../controllers/user.controller.js";
-import { authorizeById } from '../middlewares/authorization.middleware.js';
 
 const router=Router()
 
-router.route("/register").post(
-    upload.fields([
-        {
-            name: "avatar",
-            maxCount:1
-        },
-        {
-            name: "coverImage",
-            maxCount:1
-        }
-
-    ]),
-    regsiterUser)
+router.route("/register").post(regsiterUser)
 router.route("/login").post(loginUser)
 
 //secure route
@@ -43,16 +29,8 @@ router.route("/refreshToken").post(refreshToken) // refreshToken route jwt vefic
 router.route("/getCurrentUser").get(verifyJWT,getCurrentUser)
 router.route("/changeCurrentPassword").post(verifyJWT,changeCurrentPassword)
 router.route("/updateUserDetails").patch(verifyJWT,updateUserDetails)
-router.route("/updateUserAvatar").patch(
-    verifyJWT,
-    upload.single("avatar"),
-    updateUserAvatar
-)
-router.route("/updateUserCoverImage").patch(
-    verifyJWT,
-    upload.single("coverImage"),
-    updateUserCoverImage
-)
+router.route("/updateUserAvatar").patch(verifyJWT,updateUserAvatar)
+router.route("/updateUserCoverImage").patch(verifyJWT,updateUserCoverImage)
 router.route("/channel/:username").get(verifyJWT,getUserChannelProfile)
 router.route("/history").get(verifyJWT,getWatchHistory)
 router.route("/").delete(verifyJWT,deleteUser)
