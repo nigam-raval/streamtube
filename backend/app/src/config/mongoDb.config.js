b4acca81c2f3aebc1de0a06f617c16979f7b0af2
nigam <244813753+nigam-raval@users.noreply.github.com>
2026-02-08 13:20:30 +0530

    app(feature): add startup delay and expand startup checks to Postgres & MinIO
    -  Implemented an initial wait period to allow dependent services to fully initialize
    - Added PostgreSQL and MinIO to the startup validation sequence (previously MongoDB only)
    - Updated .env and .env.k8s with configurable delay settings.

--- backend/app/src/config/mongoDb.config.js
+++ backend/app/src/config/mongoDb.config.js
@@ -3,8 +3,7 @@ import mongoose from "mongoose";
 
 const connectMongoDb= async ()=>{
     try{
-        const connectionInstance =await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}?authSource=${process.env.AUTH_SOURCE}`)
-        console.log( `\n mongoDB is connected !! ${connectionInstance.connection.host}`)
+        await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}?authSource=${process.env.AUTH_SOURCE}`)
     }catch(error){
         console.error("MONGODB connection error",error);
         process.exit(1);
--- backend/app/src/index.js
+++ backend/app/src/index.js
@@ -1,16 +1,41 @@
 import dotenv from 'dotenv';
-import connectMongoDb from "./config/mongoDb.config.js";
 import { app } from './app.js';
+import connectMongoDb from "./config/mongoDb.config.js";
+import prisma from './config/postgres.config.js';
+import {s3Client} from './config/s3.config.js';
+import { HeadBucketCommand } from "@aws-sdk/client-s3";
 
 dotenv.config({ path: './.env', quiet: true })
 
 
-connectMongoDb()
-.then(()=>{
-    app.listen(process.env.PORT||8000,()=>{
-        console.log(`server is running on ${process.env.PORT}`)
-    })
-})
-.catch((err)=>{
-    console.log("mongo DB connection failed !!!",err)
-})
+const startServer = async () => {
+
+    try {
+        console.log("Attempting to connect to services")
+        
+        await connectMongoDb();
+        console.log("MongoDB Connected")
+        
+        await prisma.$connect();
+        console.log("Postgres (Prisma) Connected");
+        
+        await s3Client.send(new HeadBucketCommand({Bucket: process.env.STORAGE_BUCKET }));
+        console.log("MinIO (S3) Connected");
+        
+        app.listen(process.env.PORT||8000,()=>{
+            console.log(`server is running on ${process.env.PORT}`)
+        })
+
+    } catch (error) {
+        console.error("Startup Error:", error);
+        await prisma.$disconnect();
+        process.exit(1);            
+    }
+
+}
+
+const delay=process.env.STARTUP_DELAY
+console.log(`Sleeping for ${delay/1000}s to let other services to boot up`)
+setTimeout(() => {
+    startServer()
+}, delay);
