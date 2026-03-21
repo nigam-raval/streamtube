5d9e3f979d9929a8d99db96f84e2c6b67ffc8739
nigam <244813753+nigam-raval@users.noreply.github.com>
2026-03-22 02:14:01 +0530

    app(feature): add liveness and readiness probe in healthcheck - rename /healthcheck into /healthcheck/live for liveness - add /healthcheck/ready to verify dependent services (postgres, mongodb, s3(minio)) - update compose.yaml to use new liveness path - update app.yaml to use /healthcheck/live and /healthcheck/ready in k8s

--- backend/app/src/controllers/healthcheck.controller.js
+++ backend/app/src/controllers/healthcheck.controller.js
@@ -1,8 +1,62 @@
 import { ApiResponse } from '../utils/ApiResponse.js'
 import { asyncHandler } from '../utils/asyncHandler.js'
+import mongoose from 'mongoose'
+import prisma from '../config/postgres.config.js'
+import { s3Client } from '../config/s3.config.js'
+import { HeadBucketCommand } from '@aws-sdk/client-s3'
 
-const healthcheck = asyncHandler(async (req, res) => {
+const liveness = asyncHandler(async (req, res) => {
   return res.status(200).json(new ApiResponse(200, 'server is healthy'))
 })
 
-export { healthcheck }
+const readiness = asyncHandler(async (req, res) => {
+  let mongodbConnected
+  try {
+    await mongoose.connection.db.admin().ping()
+    mongodbConnected = true
+  } catch {
+    mongodbConnected = false
+  }
+
+  let postgresConnected
+  try {
+    await prisma.$queryRaw`SELECT 1`
+    postgresConnected = true
+  } catch {
+    postgresConnected = false
+  }
+
+  let s3connected
+  try {
+    await s3Client.send(new HeadBucketCommand({ Bucket: process.env.STORAGE_BUCKET }))
+    s3connected = true
+  } catch {
+    s3connected = false
+  }
+
+  const healthy = mongodbConnected && postgresConnected && s3connected
+
+  let statusCode
+  let message
+
+  if (healthy) {
+    statusCode = 200
+    message = 'Success'
+  } else {
+    statusCode = 503
+    message = 'Error'
+  }
+
+  const response = {
+    status: healthy ? 'pass' : 'fail',
+    detail: {
+      mongodb: mongodbConnected ? 'up' : 'down',
+      postgres: postgresConnected ? 'up' : 'down',
+      s3: s3connected ? 'up' : 'down',
+    },
+  }
+
+  return res.status(statusCode).json(new ApiResponse(statusCode, response, message))
+})
+
+export { liveness, readiness }
