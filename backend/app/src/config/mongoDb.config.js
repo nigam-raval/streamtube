b9cc42a0e8adea3c19d1ddb505d33dec0452b265
nigam <244813753+nigam-raval@users.noreply.github.com>
2026-04-03 07:28:30 +0530

    rename AUTH_SOURCE to MONGODB_AUTH_SOURCE

--- backend/app/src/config/mongoDb.config.js
+++ backend/app/src/config/mongoDb.config.js
@@ -2,9 +2,12 @@ import mongoose from 'mongoose'
 import { env } from './env.config.js'
 
 const connectMongoDb = async () => {
-  await mongoose.connect(`${env.MONGODB_URI}/${env.DB_NAME}?authSource=${env.AUTH_SOURCE}`, {
-    serverSelectionTimeoutMS: 5000,
-  })
+  await mongoose.connect(
+    `${env.MONGODB_URI}/${env.MONGODB_NAME}?authSource=${env.MONGODB_AUTH_SOURCE}`,
+    {
+      serverSelectionTimeoutMS: 5000,
+    }
+  )
 }
 
 export default connectMongoDb
