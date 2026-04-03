c3c30a2cfffe6657813a01bc5c28d8587e31a9ee
nigam <244813753+nigam-raval@users.noreply.github.com>
2026-04-03 06:20:24 +0530

    fix formatting

--- backend/app/src/config/mongoDb.config.js
+++ backend/app/src/config/mongoDb.config.js
@@ -2,10 +2,9 @@ import mongoose from 'mongoose'
 import { env } from './env.config.js'
 
 const connectMongoDb = async () => {
-  await mongoose.connect(
-    `${env.MONGODB_URI}/${env.DB_NAME}?authSource=${env.AUTH_SOURCE}`,
-    { serverSelectionTimeoutMS: 5000 }
-  )
+  await mongoose.connect(`${env.MONGODB_URI}/${env.DB_NAME}?authSource=${env.AUTH_SOURCE}`, {
+    serverSelectionTimeoutMS: 5000,
+  })
 }
 
 export default connectMongoDb
