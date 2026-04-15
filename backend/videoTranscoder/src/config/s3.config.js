5e43555844998e58eb4b0dfc79469331d9a23833
nigam <244813753+nigam-raval@users.noreply.github.com>
2026-04-15 20:35:01 +0530

    rename `s3` to `s3Client` for consistent naming across microservice

--- backend/videoTranscoder/src/config/s3.config.js
+++ backend/videoTranscoder/src/config/s3.config.js
@@ -1,7 +1,7 @@
 import { S3Client } from '@aws-sdk/client-s3'
 import { env } from './env.config.js'
 
-export const s3 = new S3Client({
+export const s3Client = new S3Client({
   region: env.STORAGE_REGION,
   credentials: {
     accessKeyId: env.STORAGE_ACCESS_KEY,
