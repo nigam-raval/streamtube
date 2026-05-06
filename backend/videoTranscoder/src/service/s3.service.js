74f74ebdb7f19810465285fbebe743997a84f534
Nigam Raval <244813753+nigam-raval@users.noreply.github.com>
2026-05-06 23:42:11 +0530

    Refactor(videoTranscoder): rename variables (#12)
    rename `videoTranscoder` variables for consistency and explicitness
    
    - renamed `RABBITMQ_QUEUE` to `RABBITMQ_VIDEO_PROCESSING_QUEUE` to make it more explicit
    - rename `s3` to `s3Client` for consistent naming across microservice

--- backend/videoTranscoder/src/service/s3.service.js
+++ backend/videoTranscoder/src/service/s3.service.js
@@ -2,13 +2,13 @@ import { GetObjectCommand, PutObjectCommand } from '@aws-sdk/client-s3'
 import { createWriteStream, readFileSync, readdirSync } from 'fs'
 import { join } from 'path'
 import { pipeline } from 'stream/promises'
-import { s3 } from '../config/s3.config.js'
+import { s3Client } from '../config/s3.config.js'
 import { env } from '../config/env.config.js'
 
 export async function downloadFromS3(s3Key, localPath) {
   console.log(`Downloading ${s3Key} from S3 bucket ${env.STORAGE_BUCKET}...`)
   const command = new GetObjectCommand({ Bucket: env.STORAGE_BUCKET, Key: s3Key })
-  const response = await s3.send(command)
+  const response = await s3Client.send(command)
   await pipeline(response.Body, createWriteStream(localPath))
   console.log('Download complete.')
 }
@@ -19,7 +19,9 @@ export async function uploadDirectoryToS3(localDir, s3Prefix) {
     const filePath = join(localDir, file)
     const key = join(s3Prefix, file).replace(/\\/g, '/') // Fix Windows path issue
     const fileContent = readFileSync(filePath)
-    await s3.send(new PutObjectCommand({ Bucket: env.STORAGE_BUCKET, Key: key, Body: fileContent }))
+    await s3Client.send(
+      new PutObjectCommand({ Bucket: env.STORAGE_BUCKET, Key: key, Body: fileContent })
+    )
   }
   console.log('All files uploaded to S3.')
 }
