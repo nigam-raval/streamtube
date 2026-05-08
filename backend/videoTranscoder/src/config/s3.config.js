df4ee8131da1cfa7f44cc10d9e3d4c793a008748
nigam <244813753+nigam-raval@users.noreply.github.com>
2026-05-09 00:47:35 +0530

    refactor(videoTranscoder): drop s3 service dependency on global env for standalone s3 config - Drop direct dependency on env.config.js - add s3Config object which contain configuration properties - Use s3.config.js as sole source of truth for rabbitmq.service.js

--- backend/videoTranscoder/src/config/s3.config.js
+++ backend/videoTranscoder/src/config/s3.config.js
@@ -1,7 +1,7 @@
 import { S3Client } from '@aws-sdk/client-s3'
 import { env } from './env.config.js'
 
-export const s3Client = new S3Client({
+export const s3Config = {
   region: env.STORAGE_REGION,
   credentials: {
     accessKeyId: env.STORAGE_ACCESS_KEY,
@@ -10,4 +10,16 @@ export const s3Client = new S3Client({
   requestTimeout: 360000,
   endpoint: env.STORAGE_ENDPOINT || undefined,
   forcePathStyle: env.STORAGE_FORCE_PATH_STYLE,
+  bucket: env.STORAGE_BUCKET,
+}
+
+export const s3Client = new S3Client({
+  region: s3Config.region,
+  credentials: {
+    accessKeyId: s3Config.credentials.accessKeyId,
+    secretAccessKey: s3Config.credentials.secretAccessKey,
+  },
+  requestTimeout: s3Config.requestTimeout,
+  endpoint: s3Config.endpoint,
+  forcePathStyle: s3Config.forcePathStyle,
 })
