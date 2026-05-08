8c94ede61b01df212d24e4d455245864593bcd28
nigam <244813753+nigam-raval@users.noreply.github.com>
2026-05-08 17:09:48 +0530

    refactor(videoTranscoder): drop rabbitmq service dependency on global env for standalone rabbitmq config - Drop direct dependency on env.config.js - Create config/rabbitmq.config.js   - add rabbitmqConfig object which contain configuration properties   - Centralize connection logic into a connectRabbitMQ function - Use rabbitmq.config.js as sole source of truth for rabbitmq.service.js - Update index.js to utilize connectRabbitMQ and inject dependencies into service

--- backend/videoTranscoder/src/index.js
+++ backend/videoTranscoder/src/index.js
@@ -1,76 +1,74 @@
 import dotenv from 'dotenv'
 import { extname } from 'path'
-import { downloadFromS3, uploadDirectoryToS3 } from './service/s3.service.js'
-import { processHLS } from './service/ffmpeg.service.js'
+import { fileTypeFromFile } from 'file-type'
 import { generateOutputPrefix } from './utils/generateOutputPrefix.utils.js'
 import { checkDirectoriesExist } from './utils/checkDirectoriesExist.utils.js'
-import { fetchOneRabbitmqMessage, sendRabbitmqAck } from './service/rabbitmq.service.js'
 import { removeBucketName } from './utils/removeBucketName.js'
-import { fileTypeFromFile } from 'file-type'
+import { downloadFromS3, uploadDirectoryToS3 } from './service/s3.service.js'
+import { processHLS } from './service/ffmpeg.service.js'
+import { connectRabbitMQ } from './config/rabbitmq.config.js'
+import { fetchOneRabbitmqMessage, sendRabbitmqAck } from './service/rabbitmq.service.js'
 
 dotenv.config({ quiet: true })
 ;(async () => {
   try {
     console.log('Starting Video Worker')
 
-    const RabbitMQ = await fetchOneRabbitmqMessage()
-    const { msg, channel, connection, key } = RabbitMQ
+    const { connection, channel } = await connectRabbitMQ()
+    if (!connection || !channel) {
+      console.error('Error: RabbitMQ is not connected, exiting ')
+      process.exit(1)
+    }
+    console.log('Rabbitmq is connected')
+
+    const { msg, key } = await fetchOneRabbitmqMessage(connection, channel)
+    if (!msg || !key) {
+      console.error('Error: queue is empty,exiting ')
+      process.exit(1)
+    }
     const S3_INPUT_KEY = removeBucketName(key)
     const S3_OUTPUT_PREFIX = generateOutputPrefix(S3_INPUT_KEY) // S3 prefix for upload
 
-    if (!RabbitMQ) {
-      console.error("Error: rabbitmq message didn't received")
-      process.exit(1)
-    }
-
-    // Validation: Check if the file is .mp4 before proceeding
     const fileExtension = extname(S3_INPUT_KEY).toLowerCase()
     if (fileExtension !== '.mp4') {
       console.error('Error: Only MP4 files are supported. The provided file is not an MP4.')
-      await sendRabbitmqAck(msg, channel, connection)
+      await sendRabbitmqAck(connection, channel, msg)
       process.exit(1)
     }
 
-    // check if required directories is exist, if not then creating it
+    // check if required directories is exist, if not then  it will create it
     checkDirectoriesExist()
 
     // Configuration
     const LOCAL_DOWNLOAD_PATH = './tmp/input/video.mp4' // Local path for download (without extension)
     const LOCAL_OUTPUT_PATH = './tmp/output' // Local output path
 
-    // Download from S3 to local
     await downloadFromS3(S3_INPUT_KEY, LOCAL_DOWNLOAD_PATH)
 
-    // Verifying Magic Bytes to ensure that virus can not process and only .mp4 get process via ffmpeg
+    // Verifying Magic Bytes to ensure that malicious file can not process and only .mp4 get process via ffmpeg
     console.log('Verifying Magic Bytes')
 
-    // Reads the binary signature of the downloaded file
     const fileType = await fileTypeFromFile(LOCAL_DOWNLOAD_PATH)
 
-    // check if file is video/mp4
     if (!fileType || fileType.mime !== 'video/mp4') {
       console.error('Security Alert: File Signature Mismatch')
       console.error(`Expected: video/mp4, Detected: ${fileType?.mime || 'Unknown'}`)
 
-      //send Rabbitmq Ack so virus or fake .mp4 did not reprocess
-      await sendRabbitmqAck(msg, channel, connection)
+      //send Rabbitmq Ack so malicious file or fake .mp4 did not reprocess
+      await sendRabbitmqAck(connection, channel, msg)
 
       process.exit(1)
     }
 
-    // Process the video into HLS format
     console.log('Starting Transcoding')
     await processHLS(LOCAL_DOWNLOAD_PATH, LOCAL_OUTPUT_PATH)
 
-    // Upload the output to S3
     console.log('Starting upload Directory')
     await uploadDirectoryToS3(LOCAL_OUTPUT_PATH, S3_OUTPUT_PREFIX)
 
-    // send ack to rabbitmq
     console.log('Sending Rabbitmq Ack')
-    await sendRabbitmqAck(msg, channel, connection)
+    await sendRabbitmqAck(connection, channel, msg)
 
-    //proper exit
     console.log('Job Completed Successfully')
     process.exit(0)
   } catch (err) {
