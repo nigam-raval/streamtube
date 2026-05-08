ca424698de9190fc562f23a446fcfbbfffb74fcf
nigam <244813753+nigam-raval@users.noreply.github.com>
2026-05-09 00:47:35 +0530

    refactor(videoTranscoder): drop s3 service dependency on global env for standalone s3 config - Drop direct dependency on env.config.js - add s3Config object which contain configuration properties - Use s3.config.js as sole source of truth for rabbitmq.service.js

--- backend/videoTranscoder/src/service/s3.service.js
+++ backend/videoTranscoder/src/service/s3.service.js
@@ -2,12 +2,11 @@ import { GetObjectCommand, PutObjectCommand } from '@aws-sdk/client-s3'
 import { createWriteStream, readFileSync, readdirSync } from 'fs'
 import { join } from 'path'
 import { pipeline } from 'stream/promises'
-import { s3Client } from '../config/s3.config.js'
-import { env } from '../config/env.config.js'
+import { s3Client, s3Config } from '../config/s3.config.js'
 
 export async function downloadFromS3(s3Key, localPath) {
-  console.log(`Downloading ${s3Key} from S3 bucket ${env.STORAGE_BUCKET}...`)
-  const command = new GetObjectCommand({ Bucket: env.STORAGE_BUCKET, Key: s3Key })
+  console.log(`Downloading ${s3Key} from S3 bucket: ${s3Config.bucket}`)
+  const command = new GetObjectCommand({ Bucket: s3Config.bucket, Key: s3Key })
   const response = await s3Client.send(command)
   await pipeline(response.Body, createWriteStream(localPath))
   console.log('Download complete.')
@@ -17,10 +16,10 @@ export async function uploadDirectoryToS3(localDir, s3Prefix) {
   const files = readdirSync(localDir)
   for (const file of files) {
     const filePath = join(localDir, file)
-    const key = join(s3Prefix, file).replace(/\\/g, '/') // Fix Windows path issue
+    const key = join(s3Prefix, file)
     const fileContent = readFileSync(filePath)
     await s3Client.send(
-      new PutObjectCommand({ Bucket: env.STORAGE_BUCKET, Key: key, Body: fileContent })
+      new PutObjectCommand({ Bucket: s3Config.bucket, Key: key, Body: fileContent })
     )
   }
   console.log('All files uploaded to S3.')
